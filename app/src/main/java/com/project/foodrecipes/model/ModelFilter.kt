f8c9d1842d085c99ae247d845f0a5b0f43d85857
palistha maharjan <58799140+palisthamaharjan123@users.noreply.github.com>
2023-03-09 20:37:33 +0545

    android project

---
+++ app/src/main/java/com/project/foodrecipes/model/ModelFilter.kt
@@ -0,0 +1,16 @@
+package com.project.foodrecipes.model
+
+import java.io.Serializable
+
+
+class ModelFilter : Serializable {
+
+    var idMeal: String? = null
+
+    @JvmField
+    var strMeal: String? = null
+
+    @JvmField
+    var strMealThumb: String? = null
+
+}
---
+++ app/src/main/java/com/project/foodrecipes/networking/Api.kt
@@ -0,0 +1,11 @@
+package com.project.foodrecipes.networking
+
+
+
+object Api {
+
+    var Categories      = "https://www.themealdb.com/api/json/v1/1/categories.php"
+    var Filter          = "https://www.themealdb.com/api/json/v1/1/filter.php?c={strCategory}"
+    var DetailRecipe    = "https://www.themealdb.com/api/json/v1/1/lookup.php?i={idMeal}"
+
+}
